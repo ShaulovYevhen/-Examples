896cecfbf02d5b2911697d07695d8e87710d25e4
GeeJaaY <gejay91@gmail.com>
2020-01-02 19:38:38 +0200

    Variables

---
+++ cd/starter/L1/Variables/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string uberflu?;
+            int _Identifier;
+            float \u006fIdentifier;
+            int &myVar;
+            string myVariab1le;
+
+        }
+    }
+}
