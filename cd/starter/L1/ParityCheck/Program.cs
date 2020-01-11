981bb8107ed0c24a96ad33bd4e27d365f19d366c
GeeJaaY <gejay91@gmail.com>
2020-01-11 12:43:30 +0200

    ParityCheck

---
+++ cd/starter/L1/ParityCheck/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace ParityCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Введите целое число");
+            while (!int.TryParse(Console.ReadLine(), out n))
+            {
+                Console.WriteLine("Ошибка ввода! Введите целое число");
+            }
+
+        }
+    }
+}
