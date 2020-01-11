9cf067d0a707d717ecb8337267d2936a497f0098
GeeJaaY <gejay91@gmail.com>
2020-01-11 12:01:15 +0200

    powerOfTwo

--- cd/starter/L1/powersOfTwo/Program.cs
+++ cd/starter/L1/powersOfTwo/Program.cs
@@ -5,6 +5,21 @@ namespace powersOfTwo
     {
         static void Main(string[] args)
         {
+            int n;
+            Console.WriteLine("Введите целое число");
+            while (!int.TryParse(Console.ReadLine(), out n))
+            {
+                Console.WriteLine("Ошибка ввода! Введите целое число");
+            }
+            if ((n & (n - 1)) == 0)
+            {
+                Console.WriteLine("является");
+            }
+            else
+            {
+                Console.WriteLine("не является");
+            }
+            Console.ReadKey();
         }
     }
 }
