bcb73642155061fb0f40cd3a366bb4bb1a522ee0
GeeJaaY <gejay91@gmail.com>
2020-01-11 12:32:19 +0200

    DeMorgan

--- cd/starter/L1/DeMorganTheorem/Program.cs
+++ cd/starter/L1/DeMorganTheorem/Program.cs
@@ -5,19 +5,20 @@ namespace DeMorganTheorem
     {
         static void Main(string[] args)
         {
-            bool A = true, B = false;
+            bool A = true,
+                 B = false;
             Console.WriteLine("Условие до применения теоремы Де Моргана");
             if (A | B)
                 Console.WriteLine("A | B = {0}", A | B);
             else
                 Console.WriteLine("A | B = {0}", A | B);
-
             Console.WriteLine("Условие после применения теоремы Де Моргана");
             if (!(!A & !B))
                 Console.WriteLine("!(!A & !B) = {0}", !(!A & !B));
             else
                 Console.WriteLine("!(!A & !B) = {0}", !(!A & !B));
             Console.ReadKey();
+
         }
     }
 }
