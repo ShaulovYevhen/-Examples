fc8cccb92e930b837b2826329c585cc484a0def1
GeeJaaY <gejay91@gmail.com>
2020-01-14 20:07:49 +0200

    CurrencyConversion

--- cd/starter/L1/CurrencyConversionApp/Program.cs
+++ cd/starter/L1/CurrencyConversionApp/Program.cs
@@ -4,8 +4,29 @@ namespace CurrencyConversionApp
 {
     class Program
     {
+        static float Input()
+        {
+            float Num;
+            while (!float.TryParse(Console.ReadLine(), out Num))
+            {
+                Console.WriteLine("Error!Enter number");
+            }
+            return Num;
+        }
+
+        static void converter(float amounth, float exchangeRate)
+        {
+            Console.WriteLine(amounth * exchangeRate);
+        }
         static void Main(string[] args)
         {
+            float amounth, exchangeRate;
+            Console.WriteLine("Enter the amount of money you need to transfer: ");
+            amounth = Input();
+            Console.WriteLine("Enter exchange rate: ");
+            exchangeRate = Input();
+            converter(amounth,exchangeRate);
+            Console.ReadKey();
         }
     }
 }
