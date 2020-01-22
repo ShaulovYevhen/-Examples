df2987c31ab6b44e4f2313109ba6edceae98043c
GeeJaaY <gejay91@gmail.com>
2020-01-22 16:32:58 +0200

    ArrayApp

--- cd/starter/L1/ArrayApp/Program.cs
+++ cd/starter/L1/ArrayApp/Program.cs
@@ -14,38 +14,69 @@ namespace ArrayApp
             }
             return number;
         }
-        
-        static void Main(string[] args)
+
+        static int[] ArrayCreate(int arrayLenght)
         {
-            int n = Input();
-            int[] array = new int[n];
+            int[] array = new int[arrayLenght];
             Random rand = new Random();
-          
-            
             for (int i = 0; i < array.Length; i++)
             {
-                array[i] = rand.Next(1 , 100);
-            } 
+                array[i] = rand.Next(1, 100);
+            }
+            return array;
+        }
 
-            int max = array[0], min = array[0], sum = 0;
-            Console.WriteLine("Нечётные значения : \n");
+        static int SumArray(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            return sum;
+        }
+
+        static int MaxInArray(int[] array)
+        {
+            int max = array[0];
             for (int i = 0; i < array.Length; i++)
             {
                 max = Math.Max(array[i], max);
+            }
+
+            return max;
+        }
+        static int MinInArray(int[] array)
+        {
+            int min = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
                 min = Math.Min(array[i], min);
+            }
+
+            return min;
+        }
+
+        static void OddValues(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
                 if (array[i] % 2 != 0)
                 {
                     Console.Write(array[i] + " ");
                 }
-
-                Console.Write(array[i] + " ");
-                sum += array[i];
             }
+        }
 
-            Console.WriteLine("\nСреднее арифметическое : " + sum/n);
-            Console.WriteLine("Сумма всех елементов : " + sum);
-            Console.WriteLine("Минимальный елемент : " + min);
-            Console.WriteLine("Максимальный елемент : " + max);
+        static void Main(string[] args)
+        {
+            int[] array = ArrayCreate(Input());
+            Console.WriteLine("Нечётные значения : \n");
+            OddValues(array);
+            Console.WriteLine("\nСреднее арифметическое : " + (SumArray(array)/array.Length));
+            Console.WriteLine("Сумма всех елементов : " + SumArray(array));
+            Console.WriteLine("Минимальный елемент : " + MinInArray(array));
+            Console.WriteLine("Максимальный елемент : " + MaxInArray(array));
             Console.ReadKey();
         }
     }
