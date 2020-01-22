b8c18d2301a822c907260072b8e695c0c83cb6c6
GeeJaaY <gejay91@gmail.com>
2020-01-22 15:09:24 +0200

    ArrayApp

--- cd/starter/L1/ArrayApp/Program.cs
+++ cd/starter/L1/ArrayApp/Program.cs
@@ -4,8 +4,49 @@ namespace ArrayApp
 {
     class Program
     {
+        static int Input()
+        {
+            int number;
+            Console.WriteLine("Введите размер массива");
+            while (!int.TryParse(Console.ReadLine(), out number))
+            {
+                Console.WriteLine("Error!Enter number");
+            }
+            return number;
+        }
+        
         static void Main(string[] args)
         {
+            int n = Input();
+            int[] array = new int[n];
+            Random rand = new Random();
+          
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(1 , 100);
+            } 
+
+            int max = array[0], min = array[0], sum = 0;
+            Console.WriteLine("Нечётные значения : \n");
+            for (int i = 0; i < array.Length; i++)
+            {
+                max = Math.Max(array[i], max);
+                min = Math.Min(array[i], min);
+                if (array[i] % 2 != 0)
+                {
+                    Console.Write(array[i] + " ");
+                }
+
+                Console.Write(array[i] + " ");
+                sum += array[i];
+            }
+
+            Console.WriteLine("\nСреднее арифметическое : " + sum/n);
+            Console.WriteLine("Сумма всех елементов : " + sum);
+            Console.WriteLine("Минимальный елемент : " + min);
+            Console.WriteLine("Максимальный елемент : " + max);
+            Console.ReadKey();
         }
     }
 }
