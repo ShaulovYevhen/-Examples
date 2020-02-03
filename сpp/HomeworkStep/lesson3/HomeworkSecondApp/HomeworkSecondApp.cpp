32fdb9cd9ae9d556d44dd264e38bc45a1f2bd3b7
GeeJaaY <gejay91@gmail.com>
2020-02-03 11:28:31 +0200

    commit

---
+++ сpp/HomeworkStep/lesson3/HomeworkSecondApp/HomeworkSecondApp.cpp
@@ -0,0 +1,66 @@
+﻿/*
+2. Компьютер загадывает число от 0 до 10.
+Задача игрока угадать число за 3 попытки.
+*/
+
+#include <iostream>
+#include <time.h>
+#include <random>
+using namespace std;
+int main()
+{
+    setlocale(0, "rus");
+    srand(time(NULL));
+    int user_choice;
+    int pc_choice = rand() % 11;
+    cout << "Угадайте число от 0 до 10, у Вас три попытки" << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        cout << "Введите число от 0 до 10" << endl;
+        cin >> user_choice;
+        while (cin.fail())
+        {
+            cin.clear();
+            cin.ignore(100, '\n');
+            cout << "Неправильный ввод" << endl;
+            cout << "Введите число от 0 до 10" << endl;
+            cin >> user_choice;
+        }
+
+        while (cin.good())
+        {
+            if (user_choice >= 0 && user_choice <= 10)
+            {
+                break;
+            }
+            else
+            {
+                cout << "Неправильный ввод" << endl;
+                cout << "Введите число от 0 до 10" << endl;
+                cin >> user_choice;
+                while (cin.fail())
+                {
+                    cin.clear();
+                    cin.ignore(100, '\n');
+                    cout << "Неправильный ввод" << endl;
+                    cout << "Введите число от 0 до 10" << endl;
+                    cin >> user_choice;
+                }
+            }
+        }
+        if (user_choice == pc_choice)
+        {
+            cout << "Вы угадали" << endl;
+            cout << "Компьютер загадал число: " << pc_choice << endl;
+            break;
+        }
+        else
+        {
+            cout << "Вы не угадали" << endl;
+            if (i == 2)
+            {
+                cout << "Компьютер загадал число: " << pc_choice << endl;
+            }
+        }
+    }
+}
---
+++ сpp/HomeworkStep/lesson3/HomeworkThirdApp/HomeworkThirdApp.cpp
@@ -0,0 +1,58 @@
+﻿/*
+3. Пользователь вводит 10 чисел, программа считает сумму первых 5
+и сумму вторых 5 чисел. Программа сравнивает полученные суммы и выводит
+сообщение - равные ли они или сумма первых больше суммы вторых 5
+*/
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	setlocale(0, "rus");
+	int sum1 = 0, sum2 = 0, num;
+	cout << "Вдедите 10 чисел" << endl;
+	for (int i = 1; i <= 10; i++)
+	{
+		if (i <= 5)
+		{
+			cout << "Введите число номер " << i << endl;
+			cin >> num;
+			while (cin.fail())
+			{
+				cin.clear();
+				cin.ignore(100, '\n');
+				cout << "Неправильный ввод" << endl;
+				cout << "Введите число номер " << i << endl;
+				cin >> num;
+			}
+			sum1 += num;
+		}
+		else
+		{
+			cout << "Введите число номер " << i << endl;
+			cin >> num;
+			while (cin.fail())
+			{
+				cin.clear();
+				cin.ignore(100, '\n');
+				cout << "Неправильный ввод" << endl;
+				cout << "Введите число номер " << i << endl;
+				cin >> num;
+			}
+			sum2 += num;
+		}
+	}
+	if (sum1 == sum2)
+	{
+		cout << "Cуммы равные " << sum1 << " = " << sum2 << endl;
+	}
+	else if (sum1 > sum2)
+	{
+		cout << "Cумма первой половины больше суммы второй " << sum1 << " > " << sum2 << endl;
+	}
+	else
+	{
+		cout << "Cумма второй половины больше суммы первой " << sum1 << " < " << sum2 << endl;
+	}
+}
